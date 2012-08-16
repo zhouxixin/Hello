c857c3d2e98f5a787e4fd8d446924ad27e058a44
Nilla <ching1212111@hotmail.com>
2012-08-16 16:39:55 +1000

    Update Hello/src/example/Hello.java
    I tried

--- Hello/src/example/Hello.java
+++ Hello/src/example/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello, githubºÃÓÃÂğ£¿");
+		System.out.println("Hello, I am Milla);
 
 	}
 
